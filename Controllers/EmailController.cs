38468131a34da307c6667e85b826604b9e7c8176
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-24 22:03:10 +0700

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,37 @@
+﻿using BaiTap4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap4.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MailInfo model)
+        {
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.From = new System.Net.Mail.MailAddress(model.From);
+            mail.To.Add(model.To);
+            mail.Subject = model.Subject;
+            mail.Body = model.Body;
+            mail.IsBodyHtml = true;
+
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+
+            smtp.Credentials = new System.Net.NetworkCredential(model.From, model.Password);
+            smtp.Send(mail);
+
+            ViewBag.Message = "Mail has been sent!";
+            return View();
+        }
+    }
+}
